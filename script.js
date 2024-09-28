ebdd293f1e57c23c46edde1d5f927c3418eed6e2
yashsvi1010 <yashsvisingh97@gmail.com>
2024-09-28 19:41:48 +0530

    added logic for checking number and dom manuplation

--- script.js
+++ script.js
@@ -12,12 +12,40 @@ const displayMessage = function (message) {
 //check button function
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
+  //   console.log(guess, typeof guess);
 
   if (!guess) {
     displayMessage("No Number!");
   } else if (guess === randomNumber) {
     displayMessage("Correct Guess!");
     document.querySelector(".number").textContent = randomNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== randomNumber) {
+    if (score > 1) {
+      displayMessage(guess > randomNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
